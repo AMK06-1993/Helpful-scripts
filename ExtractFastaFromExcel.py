505b0a767073b429ba2565a8fb41b3b889c3cb1d
AnnaKrzynowek <82043913+AnnaKrzynowek@users.noreply.github.com>
2021-11-08 16:25:42 +0100

    Update ExtractFastaFromExcel.py

--- ExtractFastaFromExcel.py
+++ ExtractFastaFromExcel.py
@@ -15,22 +15,22 @@ import pandas as pd
 import os
 import openpyxl
 
-def excel_to_fasta(self, path, org_col, sequence_col, sheet, file_name):
-    self.path = path
-    self.sequence_col = list(sequence_col.split(",")) #always only one element list
-    self.sheet = sheet
-    self.file_name = file_name
+def excel_to_fasta(path, org_col, sequence_col, sheet, file_name):
+    path = path
+    sequence_col = list(sequence_col.split(",")) #always only one element list
+    sheet = sheet
+    file_name = file_name
  
 #in case more than one org_col is provided:
     if isinstance(org_col, list):
-        self.org_col = org_col
+        org_col = org_col
     else:
-        self.org_col = list(org_col.split(","))
+        org_col = list(org_col.split(","))
  #need to merge columns from org_col (if there is more than one)
      try:
-        self.columns = self.org_col + self.sequence_col
-        excel_df = pd.read_excel(self.path, sheet_name= self.sheet, usecols= self.columns, header = 0)
-        excel_df['id'] = '>' + excel_df[self.org_col].apply(lambda x:'_'.join(x), axis=1)
+        columns = org_col + sequence_col
+        excel_df = pd.read_excel(path, sheet_name= sheet, usecols= columns, header = 0)
+        excel_df['id'] = '>' + excel_df[org_col].apply(lambda x:'_'.join(x), axis=1)
         excel_df['id'] = excel_df['id'].str.replace(" ","")
         id_col = excel_df.pop('id')
         excel_df.insert(0, 'id', id_col)
@@ -38,7 +38,7 @@ def excel_to_fasta(self, path, org_col, sequence_col, sheet, file_name):
 
         #dataframe to array
         numpy_array = excel_df.to_numpy()
-        file_name = self.file_name + '.txt'
+        file_name = file_name + '.txt'
 
         #save file as txt
         np.savetxt(fname = str(os.path.join(seq_path, file_name)), X = numpy_array, delimiter = ' ', fmt='%s')
